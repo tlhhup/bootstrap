720c0987dfd7f4c646e34675db9911b2956649dc
tlhhup <137273278@qq.com>
2016-08-19 16:37:44 +0800

    添加日志工具类

--- src/main/java/com/bootstrap/interceptor/RightFilterInterceptor.java
+++ src/main/java/com/bootstrap/interceptor/RightFilterInterceptor.java
@@ -1,5 +1,8 @@
 package com.bootstrap.interceptor;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -7,15 +10,32 @@ import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.bootstrap.entity.User;
+import com.bootstrap.util.LoggerUtils;
 
 public class RightFilterInterceptor implements HandlerInterceptor {
+
+	private static List<String> commons;
+
+	static {
+		commons = new ArrayList<String>();
+		commons.add("Login/login.hl");
+		commons.add("Login/logout.hl");
+	}
 
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
-		User user = (User) request.getSession().getAttribute("user");
-		if(user==null){
-			return false;
+		LoggerUtils.d("请求地址:"+request.getRequestURI());
+		String uri=request.getRequestURI();
+		uri=uri.substring(request.getContextPath().length()+1);
+		if(commons.contains(uri)){
+			return true;
+		}else{
+			User user = (User) request.getSession().getAttribute("user");
+			if (user == null) {
+				response.sendRedirect(request.getContextPath() + "/Login/login.hl");
+				return false;
+			}
 		}
 		return true;
 	}
---
+++ src/main/java/com/bootstrap/util/LoggerUtils.java
@@ -0,0 +1,29 @@
+package com.bootstrap.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Marker;
+import org.slf4j.MarkerFactory;
+
+public class LoggerUtils {
+	
+	private static Logger logger = LoggerFactory.getLogger(LoggerUtils.class);
+	private static Marker marker = MarkerFactory.getMarker("com.bootstrap");
+
+	public static void d(String msg) {
+		logger.debug(marker, msg);
+	}
+
+	public static void i(String msg) {
+		logger.info(marker, msg);
+	}
+
+	public static void w(String msg) {
+		logger.warn(marker, msg);
+	}
+
+	public static void e(String msg) {
+		logger.error(marker, msg);
+	}
+
+}
